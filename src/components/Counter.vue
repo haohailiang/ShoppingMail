9b26e623737d86a1ab362e4e5ef1f329ec3fee84
haohailiang <646263409@qq.com>
2017-10-08 22:28:45 +0800

    父子组件相互通信

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+	<button @click="increment">+</button>
+	<button v-on:click="decrement">-</button>
+	<p><span>child:{{num}}</span></p>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+	props:["num"],
+	data(){
+		return {};
+	},
+	methods: {
+		increment(){
+			this.$emit('listenIncre');
+		},
+		decrement(){
+			this.$emit('listenDesc');
+		},
+	}
+};
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="hello">
+	<h1>{{msg}}</h1>
+	<Counter :num="num" @listenIncre="increment" @listenDesc="decrement"></Counter>
+    <p>parent:{{num}}</p>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+import Counter from './Counter.vue';
+export default {
+	name: 'hello',
+	data() {
+		return {
+			num:100,
+			msg: 'Welcome to Your Vue.js App , test it .'
+		}
+	},
+	components:{
+		Counter
+	},
+	methods:{
+		increment(){
+			this.num++;
+		},
+		decrement(){
+			this.num--;
+		}
+	}
+}
+
+</script>
