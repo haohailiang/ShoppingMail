74b4dfba2515e8e8726ec5aa69247ee1c48130ed
haohailiang <646263409@qq.com>
2017-10-12 09:46:25 +0800

    vue基础知识

--- src/view/GoodList.vue
+++ src/view/GoodList.vue
@@ -18,18 +18,9 @@
                     <div class="filter stopPop" id="filter">
                         <dl class="filter-price">
                             <dt>Price:</dt>
-                            <dd><a href="javascript:void(0)">All</a></dd>
-                            <dd>
-                                <a href="javascript:void(0)">0 - 100</a>
-                            </dd>
-                            <dd>
-                                <a href="javascript:void(0)">100 - 500</a>
-                            </dd>
-                            <dd>
-                                <a href="javascript:void(0)">500 - 1000</a>
-                            </dd>
-                            <dd>
-                                <a href="javascript:void(0)">1000 - 2000</a>
+                            <dd><a href="javascript:void(0)" :class="{'cur':priceChecked == 'all'}" @click="priceChecked='all'">All</a></dd>
+                            <dd v-for="(item, index) in priceFilter" @click="priceChecked=index">
+                                <a href="javascript:void(0)" :class="{'cur':priceChecked==index}">{{item.startPrice}} - {{item.endPrice}}</a>
                             </dd>
                         </dl>
                     </div>
@@ -75,7 +66,26 @@
   export default {
     data(){
       return {
-       goodsList:[]
+       priceChecked:'',
+       goodsList:[],
+       priceFilter:[
+        {
+          startPrice:'0.00',
+          endPrice:'100.00'
+        },
+        {
+          startPrice:'100.00',
+          endPrice:'500.00'
+        },
+        {
+          startPrice:'500.00',
+          endPrice:'1000.00'
+        },
+        {
+          startPrice:'1000.00',
+          endPrice:'2000.00'
+        }
+       ]
       }
     },
     mounted(){
