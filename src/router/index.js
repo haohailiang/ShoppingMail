3fa211f227d072a39a563549741a9018fc9f64ee
haohailiang <646263409@qq.com>
2017-10-25 16:21:22 +0800

    购物车架构的搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodList from './../view/GoodList'
-import Image from './../view/Image'
-import Title from './../view/Title'
 import Cart from './../view/Cart'
+import Address from './../view/Address'
 
 Vue.use(Router)
 
@@ -19,6 +18,11 @@ export default new Router({
 		  path:'/cart',
 		  name:'Cart',
 		  component:Cart
+	  },
+	  {
+		  path:'/address',
+		  name:'Address',
+		  component:Address
 	  }
   ]
 })
