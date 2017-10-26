f718130145b63364aeefe5204f257873110903f3
haohailiang <646263409@qq.com>
2017-10-26 16:28:17 +0800

    修改格式和rename文件夹爱

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import GoodList from './../view/GoodList'
-import Cart from './../view/Cart'
-import Address from './../view/Address'
+import GoodList from './../views/GoodList'
+import Cart from './../views/Cart'
+import Address from './../views/Address'
+
 
 Vue.use(Router)
 
 export default new Router({
 	mode:'hash',
-  routes: [
-	  {
-		  path:'/goods',
-		  name:'GoodList',
-		  component:GoodList
-	  },
-	  {
-		  path:'/cart',
-		  name:'Cart',
-		  component:Cart
-	  },
-	  {
-		  path:'/address',
-		  name:'Address',
-		  component:Address
-	  }
-  ]
+	routes: [
+		{
+			path:'/',
+			name:'GoodList',
+			component:GoodList
+		},
+		{
+			path:'/cart',
+			name:'Cart',
+			component:Cart
+		},
+		{
+			path:'/address',
+			name:'Address',
+			component:Address
+		}
+	]
 })
